afb68c870f878980dd19d9cdfee7d02caf26095b
MallikarjunRK <mallikarjunkorti40@gmail.com>
2026-06-22 20:09:49 +0530

    Added Admin Dashboard

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -74,4 +74,22 @@ router.post("/login", async (req, res) => {
     }
 
 });
+router.get("/", async (req, res) => {
+
+    try {
+
+        const users =
+        await User.find();
+
+        res.json(users);
+
+    } catch(error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+
+});
 module.exports = router;
